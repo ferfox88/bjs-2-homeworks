397a28b64d18ac9d7aa114638f5ef6c35539e3df
Elena <ferfox@yandex.ru>
2021-12-14 15:11:10 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,44 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let x1;
+  let x2;
+  let x;
+  let d = b ** 2 - 4 * a * c;
+
+  if (d === 0) {
+    x = -b / (2 * a);
+    arr.push(x);
+  } else if (d > 0) {
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
 
+  let S = amount - contribution;
+
+  let today = new Date();
+  let month = today.getMonth();
+  let year = today.getFullYear();
+  let n = (date.getFullYear() - year) * 12 + date.getMonth() - month;
+  
+  let P = (percent / 100) / 12;
+  let payment = S * (P + (P / (((1 + P) ** n) - 1)));
+  totalAmount = parseFloat((payment * n).toFixed(2));
+  
   return totalAmount;
 }
